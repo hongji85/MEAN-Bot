a79a722c0bd31827f189c12c8adee3be47b8de59
hongji85 <hongji85@gmail.com>
2018-05-31 16:25:38 +0800

    Initial check in

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.database, { promiseLibrary: require('bluebird') })
+  .then(() => console.log(`Connected to database ${config.database}`))
+  .catch((err) => console.log(`Database error: ${err}`));
+
+const app = new express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+app.listen(port, () => {
+    console.log('server started on port', port);
+});
